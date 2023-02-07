2dcb95ceeeaec1b7486e1372b7c10771c68bde87
sedhha <activity.schoolsh2@gmail.com>
2023-02-07 18:38:45 +0530

    Added Details Until P10

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,68 +1,21 @@
-import Head from 'next/head';
-import Image from 'next/image';
+import classes from '@/styles/localStyles/Header.module.css';
 
 export default function Home() {
 	return (
-		<div>
-			<Head>
-				<title>Create Next App</title>
-				<link
-					rel='icon'
-					href='/favicon.ico'
+		<header className={classes.Header}>
+			<div className={classes.LogoWithTitle}>
+				<h1 className={classes.LogoHeader}>Person Name</h1>
+				<img
+					className={classes.Image}
+					src='/morpankh.svg'
+					alt='Logo Icon'
 				/>
-			</Head>
-
-			<main>
-				<h1>
-					Welcome to <a href='https://nextjs.org'>Next.js!</a>
-				</h1>
-
-				<p>
-					Get started by editing <code>pages/index.js</code>
-				</p>
-
-				<div>
-					<a href='https://nextjs.org/docs'>
-						<h3>Documentation &rarr;</h3>
-						<p>Find in-depth information about Next.js features and API.</p>
-					</a>
-
-					<a href='https://nextjs.org/learn'>
-						<h3>Learn &rarr;</h3>
-						<p>Learn about Next.js in an interactive course with quizzes!</p>
-					</a>
-
-					<a href='https://github.com/vercel/next.js/tree/canary/examples'>
-						<h3>Examples &rarr;</h3>
-						<p>Discover and deploy boilerplate example Next.js projects.</p>
-					</a>
-
-					<a href='https://vercel.com/new'>
-						<h3>Deploy &rarr;</h3>
-						<p>
-							Instantly deploy your Next.js site to a public URL with Vercel.
-						</p>
-					</a>
-				</div>
-			</main>
-
-			<footer>
-				<a
-					href='https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app'
-					target='_blank'
-					rel='noopener noreferrer'
-				>
-					Powered by{' '}
-					<span>
-						<Image
-							src='/vercel.svg'
-							alt='Vercel Logo'
-							width={72}
-							height={16}
-						/>
-					</span>
-				</a>
-			</footer>
-		</div>
+			</div>
+			<div className={classes.NavigationSections}>
+				<h2>About</h2>
+				<h2>Work Experience</h2>
+				<h2>Blog</h2>
+			</div>
+		</header>
 	);
 }
