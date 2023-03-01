141b2afdd8a23827ef44a0a4f1cba71cb69c70a3
sedhha <activity.schoolsh2@gmail.com>
2023-03-01 21:03:07 +0530

    Radio-Toggle Finish

--- components/RadioButton/index.tsx
+++ components/RadioButton/index.tsx
@@ -1,11 +1,25 @@
+import { useState } from 'react';
 import classes from './RadioButton.module.css';
 
+function updateValue(currValue: boolean): boolean {
+	return !currValue; // true -> false, false -> true
+}
+
+/*
+	Condition To be checked ? (value or expression if condition is true): (value or expression if condition is false)
+*/
 const RadioButton = () => {
+	const [isOn, setIsOn] = useState<boolean>(false);
 	return (
 		<div className={classes.OuterEnclosure}>
-			<section className={classes.ToggleButton} />
+			<section
+				className={[
+					classes.ToggleButton,
+					isOn ? classes.ToggleButtonLeft : classes.ToggleButtonRight,
+				].join(' ')}
+				onClick={() => setIsOn(updateValue)}
+			/>
 		</div>
 	);
 };
-
 export default RadioButton;
